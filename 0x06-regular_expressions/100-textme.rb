eb69e729feb926f4a2a2e734de981a46e2f6fae6
OrionDooms <orion45.dd@gmail.com>
2024-01-30 13:05:07 -0800

    Ruby script that accepts the argument and pass it to a regular expression

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# Ruby script that accepts one argument and
+# pass it to a regular expression
+# The script output: [SENDER],[RECEIVER],[FLAGS]    
+puts ARGV[0].scan(/[+a-zA-Z-,:0-9]+/).join
