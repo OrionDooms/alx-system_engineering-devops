884f9a110f076669d7b0f1de816c84350b13db73
OrionDooms <orion45.dd@gmail.com>
2024-02-02 05:06:59 -0800

    The script should output: [SENDER],[RECEIVER],[FLAGS]

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 # Ruby script that accepts one argument and
 # pass it to a regular expression
 # The script output: [SENDER],[RECEIVER],[FLAGS]    
-puts ARGV[0].scan(/\[from:(.*?)\]|\[to:(.*?)\]|\[flages:(.*?)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flages:(.*?)\]/).join
