3541996ede581f56f26780dda799a4df070829ad
OrionDooms <Orion45.dd@gmail.com>
2024-03-22 09:55:22 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-#Install a flask version 2.1.0 package using puppet
-package {'flask':
+#!/usr/bin/pup
+##Install a flask version 2.1.0 package using puppet
+package {'install flask':
 ensure   => '2.1.0',
+name     => 'flask',
 provider => 'pip3',
 }
