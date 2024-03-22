b1b74237eb25323c46833e7081826c9afd921e2a
OrionDooms <Orion45.dd@gmail.com>
2024-03-22 15:57:17 -0700

    need to install Werkzeng 2.1.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 #!/usr/bin/pup
-##Install a flask version 2.1.0 package using puppet
-package {'install flask':
-ensure   => '2.1.0',
-name     => 'flask',
+#Install a flask version 2.1.0 package using puppet
+package { 'flask':
+  ensure => '2.1.0',
+  name   => 'flask',
 provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
