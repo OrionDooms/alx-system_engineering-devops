96972039eec1a43694e634aae153b458c7a9020d
OrionDooms <Orion45.dd@gmail.com>
2024-03-26 04:43:17 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,6 +2,8 @@
 #show local SSH Client by create a client configuration file
 class ssh {
   file {'~/.ssh/school':
+    path                   => 'ect/ssh/ssh_config',
+    line                   => 'IdentityFile ~/.ssh/school',
     passwordAuthentication => 'no',
     notify                 => Service['ssh']
   }
