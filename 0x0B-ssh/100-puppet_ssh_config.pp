92a8e8e707b132f8016cd8a42fb9cdb0d4eb8836
Orion <Orion45.dd@gmail.com>
2024-04-06 09:26:10 -0700

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,34 +1,16 @@
 #!/usr/bin/pup
 #show local SSH Client by create a client configuration file
-<<<<<<< HEAD
-=======
-class ssh {
-  file {'~/.ssh/school':
-    path                   => 'ect/ssh/ssh_config',
-    line                   => 'IdentityFile ~/.ssh/school',
-    passwordAuthentication => 'no',
-    notify                 => Service['ssh']
-  }
->>>>>>> 96972039eec1a43694e634aae153b458c7a9020d
-
-file { 'ect/ssh/ssh_config':
-	ensure  => 'present',
-	content => 'IdentityFile ~/.ssh/school',
-	line => 'passwordAuthentication  no',
-
-
-}
-file {'/etc/ssh/ssh_config':
-  ensure => present,
-}
-
+class ssh_config {
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'passwordAuthentication no',
-}
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'passwordAuthentication no',
+  }
 
 file_line { 'Declare identity file':
-  path   => 'etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'IdentityFile ~/.ssh/school',
+  }
 }
-~  
+include ssh_config
