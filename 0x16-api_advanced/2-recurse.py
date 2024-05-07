f5e807d5967099455d0f84d84920ccec8033ef77
Orion <Orion45.dd@gmail.com>
2024-05-07 15:08:55 -0700

    api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-
+"""function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit."""
 import requests
+
 
 def recurse(subreddit, hot_list=[]):
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
+    if response.status_code == 404:
+        return None
+    else:
         result = response.json()
         data = result['data']['children']
         for a in data:
@@ -16,6 +20,4 @@ def recurse(subreddit, hot_list=[]):
         if after:
             return recurse(subreddit, hot_list=host_list)
         else:
-            return hot_list
-    else:
-        return None
+            return hot_lis
